bdb3037e57956c04cbc8659b12a7624514dd9a8e
silenthill99 <florian.graziani@sfr.fr>
2022-09-03 19:15:25 +0200

    Début du remplissage du menu MacDo

--- src/main/java/fr/silenthill99/principalplugin/inventory/holder/MacDoHolder.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/holder/MacDoHolder.java
@@ -1,6 +1,10 @@
 package fr.silenthill99.principalplugin.inventory.holder;
 
+import fr.silenthill99.principalplugin.ItemBuilder;
 import fr.silenthill99.principalplugin.inventory.SilenthillHolder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class MacDoHolder extends SilenthillHolder {
+
 }
--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/MacDoInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/MacDoInventory.java
@@ -1,9 +1,15 @@
 package fr.silenthill99.principalplugin.inventory.hook;
 
+import fr.silenthill99.principalplugin.ItemBuilder;
+import fr.silenthill99.principalplugin.Main;
 import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.holder.MacDoHolder;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class MacDoInventory extends AbstractInventory<MacDoHolder> {
     public MacDoInventory() {
@@ -13,8 +19,34 @@ public class MacDoInventory extends AbstractInventory<MacDoHolder> {
     @Override
     public void openInventory(Player p, Object... args)
     {
+        ItemStack steak = new ItemBuilder(Material.COOKED_BEEF).setLore("Prix : 10€").toItemStack();
+
         Inventory inv = createInventory(new MacDoHolder(), 36, "MacDo");
+        inv.setItem(0, steak);
         inv.setItem(inv.getSize()-1, CLOSE);
         p.openInventory(inv);
+    }
+
+    @Override
+    public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, MacDoHolder holder) {
+        switch (current.getType())
+        {
+            case COOKED_BEEF:
+            {
+                if (Main.getInstance().economy.has(player, 10.0f))
+                {
+                    player.sendMessage(ChatColor.GREEN + "Vous avez acheté un steak pour 10€");
+                    player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF));
+                    Main.getInstance().economy.withdrawPlayer(player, 10.0f);
+                }
+                else
+                {
+                    player.sendMessage(ChatColor.RED + "Vous n'avez pas assez d'argent !");
+                }
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
