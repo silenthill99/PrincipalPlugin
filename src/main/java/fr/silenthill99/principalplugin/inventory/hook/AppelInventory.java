62b5f64234d90c21e9ff958a9b78effe28c2049e
silenthill99 <florian.graziani@sfr.fr>
2022-09-08 11:32:48 +0200

    Finition du système d'appel aux urgences

--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/AppelInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/AppelInventory.java
@@ -1,9 +1,14 @@
 package fr.silenthill99.principalplugin.inventory.hook;
 
+import fr.silenthill99.principalplugin.ItemBuilder;
+import fr.silenthill99.principalplugin.Main;
 import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.InventoryManager;
 import fr.silenthill99.principalplugin.inventory.InventoryType;
 import fr.silenthill99.principalplugin.inventory.holder.AppelHolder;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
@@ -17,18 +22,66 @@ public class AppelInventory extends AbstractInventory<AppelHolder> {
     @Override
     public void openInventory(Player p, Object... args)
     {
+
+        ItemStack police = new ItemBuilder(Material.BLUE_WOOL).setName(ChatColor.DARK_BLUE + "Appeler la police").toItemStack();
+        ItemStack pompiers = new ItemBuilder(Material.RED_WOOL).setName(ChatColor.DARK_RED + "Appeler les pompiers").toItemStack();
+        ItemStack samu = new ItemBuilder(Material.GREEN_WOOL).setName(ChatColor.DARK_GREEN + "Appeler le SAMU").toItemStack();
+
         Inventory inv = createInventory(new AppelHolder(), 9, "Passer un appel");
-        inv.setItem(8, RETOUR);
+        inv.setItem(0, police);
+        inv.setItem(2, pompiers);
+        inv.setItem(4, samu);
+        inv.setItem(inv.getSize()-1, RETOUR);
         p.openInventory(inv);
     }
 
     @Override
     public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, AppelHolder holder) {
+        float x = (float) player.getLocation().getX();
+        float y = (float) player.getLocation().getY();
+        float z = (float) player.getLocation().getZ();
         switch (current.getType())
         {
             case SUNFLOWER:
+            {
                 InventoryManager.openInventory(player, InventoryType.TELEPHONE);
                 break;
+            }
+            case BLUE_WOOL:
+            {
+                player.closeInventory();
+                player.sendMessage(ChatColor.DARK_BLUE + "Vous avez appelé la police. Veuillez rester sur place jusqu'à leur arrivée");
+                for (Player players : Bukkit.getOnlinePlayers()) {
+                    if (Main.isPlayerInGroup(player, "policier")) {
+                        players.sendMessage(ChatColor.DARK_BLUE + "Une personne du nom de " + ChatColor.YELLOW + player.getName() + ChatColor.DARK_BLUE + " vous attend aux coordonnées " + ChatColor.YELLOW + "x: " + String.format("%.2f",x) + " y: " + String.format("%.2f", y) + " z: " + String.format("%.2f", z));
+                    }
+                }
+                Main.getInstance().logs.get(player.getUniqueId()).add(ChatColor.DARK_BLUE + player.getName() + ChatColor.BLUE + " a appelé la police aux coordonnées " + ChatColor.AQUA + "x: " + String.format("%.2f", x) + " y: " + String.format("%.2f", y) + " z: " + String.format("%.2f", z));
+                break;
+            }
+            case RED_WOOL:
+            {
+                player.closeInventory();
+                player.sendMessage(ChatColor.DARK_RED + "Vous venez de passer un appel aux pompiers. Veuillez rester à votre position jusqu'à l'arrivée des secours !");
+                for (Player players : Bukkit.getOnlinePlayers()) {
+                    if (Main.isPlayerInGroup(players, "pompier")) {
+                        players.sendMessage(ChatColor.DARK_RED + "Vous venez de recevoir un appel de " + ChatColor.YELLOW + player.getName() + ChatColor.DARK_RED + ", il se trouve aux coordonnées " + ChatColor.YELLOW + "x: " + String.format("%.2f",x) + " y: " + String.format("%.2f",y) + " z: " + String.format("%.2f",z));
+                    }
+                }
+                Main.getInstance().logs.get(player.getUniqueId()).add(ChatColor.DARK_BLUE + player.getName() + ChatColor.BLUE + " a appelé les pompiers aux coordonnées " + ChatColor.AQUA + "x: " + String.format("%.2f", x) + " y: " + String.format("%.2f", y) + " z: " + String.format("%.2f", z));
+                break;
+            }
+            case GREEN_WOOL:
+            {
+                player.closeInventory();
+                player.sendMessage(ChatColor.DARK_GREEN + "Vous venez de lancer un appel au SAMU. Veuillez rester sur place jusqu'à l'arrivée des secours !");
+                for (Player players : Bukkit.getOnlinePlayers())
+                {
+                    players.sendMessage(ChatColor.DARK_GREEN + "Une personne du nom de " + ChatColor.YELLOW + player.getName() + ChatColor.DARK_GREEN + " vous attend aux coordonnées " + ChatColor.YELLOW + "x: " + String.format("%.2f",x) + " y: " + String.format("%.2f",y) + " z: " + String.format("%.2f",z));
+                }
+                Main.getInstance().logs.get(player.getUniqueId()).add(ChatColor.DARK_BLUE + player.getName() + ChatColor.BLUE + " a appelé le SAMU aux coordonnées " + ChatColor.AQUA + "x: " + String.format("%.2f", x) + " y: " + String.format("%.2f", y) + " z: " + String.format("%.2f", z));
+                break;
+            }
             default:
                 break;
         }
--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/TelephoneInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/TelephoneInventory.java
@@ -5,9 +5,11 @@ import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.InventoryManager;
 import fr.silenthill99.principalplugin.inventory.InventoryType;
 import fr.silenthill99.principalplugin.inventory.holder.TelephoneHolder;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
@@ -22,10 +24,12 @@ public class TelephoneInventory extends AbstractInventory<TelephoneHolder> {
     @Override
     public void openInventory(Player p, Object... args) {
 
-        ItemStack appel = new ItemBuilder(Material.YELLOW_WOOL).setName(ChatColor.YELLOW + "Passer un appel").toItemStack();
+        ItemStack appel = new ItemBuilder(Material.BRICK).setName(ChatColor.YELLOW + "Passer un appel").toItemStack();
+        ItemStack argent = new ItemBuilder(Material.GOLD_INGOT).setName(ChatColor.GREEN + "Voir son argent").toItemStack();
 
         Inventory inv = createInventory(new TelephoneHolder(), 27, "Téléphone");
         inv.setItem(0, appel);
+        inv.setItem(1, argent);
         inv.setItem(inv.getSize()-1, CLOSE);
         p.openInventory(inv);
     }
@@ -34,8 +38,12 @@ public class TelephoneInventory extends AbstractInventory<TelephoneHolder> {
     public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, TelephoneHolder holder) {
         switch(current.getType())
         {
-            case YELLOW_WOOL:
+            case BRICK:
                 InventoryManager.openInventory(player, InventoryType.APPEL);
+                break;
+            case GOLD_INGOT:
+                player.closeInventory();
+                Bukkit.dispatchCommand(player, "money");
                 break;
             default:
                 break;
@@ -46,11 +54,12 @@ public class TelephoneInventory extends AbstractInventory<TelephoneHolder> {
     public void onInteract(PlayerInteractEvent e) {
         Player player = e.getPlayer();
         ItemStack it = e.getItem();
+        Action action = e.getAction();
         if (it == null)
         {
             return;
         }
-        if (it.getType().equals(Material.BRICK) && it.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.YELLOW + "Téléphone"))
+        if (it.getType().equals(Material.BRICK) && (action.equals(Action.RIGHT_CLICK_BLOCK) || action.equals(Action.RIGHT_CLICK_AIR)))
         {
             InventoryManager.openInventory(player, InventoryType.TELEPHONE);
         }
