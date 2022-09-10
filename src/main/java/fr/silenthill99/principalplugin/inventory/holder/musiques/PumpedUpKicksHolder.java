eee7c8ea70b5804131e119ad91c5bb4bae5d2e4d
silenthill99 <florian.graziani@sfr.fr>
2022-09-10 18:58:25 +0200

    Suppression de holders inutiles

--- src/main/java/fr/silenthill99/principalplugin/inventory/holder/musiques/PumpedUpKicksHolder.java
+++
@@ -1,7 +0,0 @@
-package fr.silenthill99.principalplugin.inventory.holder.musiques;
-
-import fr.silenthill99.principalplugin.inventory.SilenthillHolder;
-
-public class PumpedUpKicksHolder extends SilenthillHolder
-{
-}
