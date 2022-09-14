6e9b071801154c387ca96bbf4372bd2f5f864fa1
silenthill99 <florian.graziani@sfr.fr>
2022-09-14 19:18:46 +0200

    Ajout du système de cooldown

--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/KitInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/KitInventory.java
@@ -1,14 +1,19 @@
 package fr.silenthill99.principalplugin.inventory.hook;
 
 import fr.silenthill99.principalplugin.ItemBuilder;
+import fr.silenthill99.principalplugin.Main;
 import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.holder.KitHolder;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class KitInventory extends AbstractInventory<KitHolder> {
     public KitInventory() {
@@ -29,9 +34,19 @@ public class KitInventory extends AbstractInventory<KitHolder> {
         switch (current.getType())
         {
             case GREEN_WOOL:
+                HashMap<UUID, Double> cooldown = new HashMap<>();
+
+                if (cooldown.containsKey(player.getUniqueId()))
+                {
+                    player.sendMessage(ChatColor.RED + "Il reste encore " + (System.currentTimeMillis() - cooldown.get(player.getUniqueId())));
+                    return;
+                }
+
                 ItemStack telephone = new ItemBuilder(Material.BRICK).setName(ChatColor.YELLOW + "Téléphone").toItemStack();
                 player.closeInventory();
                 player.getInventory().addItem(telephone);
+                cooldown.put(player.getUniqueId(), (double) (System.currentTimeMillis() + 1000*60*25));
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> cooldown.remove(player.getUniqueId()), 20 * 60 * 25);
                 break;
             default:
                 break;
