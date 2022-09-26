67ef5d4b978775b178e23dacab078e2367bcc187
silenthill99 <florian.graziani@sfr.fr>
2022-09-26 23:47:38 +0200

    Ajout d'un 1er métier illégal

--- src/main/java/fr/silenthill99/principalplugin/inventory/holder/IllegauxHolder.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/holder/IllegauxHolder.java
@@ -1,6 +1,10 @@
 package fr.silenthill99.principalplugin.inventory.holder;
 
 import fr.silenthill99.principalplugin.inventory.SilenthillHolder;
+import fr.silenthill99.principalplugin.inventory.hook.IllegauxInventory;
+
+import java.util.HashMap;
 
 public class IllegauxHolder extends SilenthillHolder {
+    public HashMap<Integer, IllegauxInventory.Metiers> metiers = new HashMap<>();
 }
--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/IllegauxInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/IllegauxInventory.java
@@ -1,10 +1,17 @@
 package fr.silenthill99.principalplugin.inventory.hook;
 
+import fr.silenthill99.principalplugin.ItemBuilder;
 import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.holder.IllegauxHolder;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Locale;
 
 public class IllegauxInventory extends AbstractInventory<IllegauxHolder> {
     public IllegauxInventory() {
@@ -14,8 +21,50 @@ public class IllegauxInventory extends AbstractInventory<IllegauxHolder> {
     @Override
     public void openInventory(Player p, Object... args)
     {
+        IllegauxHolder holder = new IllegauxHolder();
+
         Inventory inv = createInventory(new IllegauxHolder(), 27, ChatColor.DARK_RED + "Métiers illégaux");
+        int slot = 0;
+        for (Metiers metiers : Metiers.values())
+        {
+            holder.metiers.put(slot, metiers);
+            inv.setItem(slot++, new ItemBuilder(Material.PAPER).setName(ChatColor.DARK_RED + metiers.name).toItemStack());
+        }
         inv.setItem(inv.getSize()-1, CLOSE);
         p.openInventory(inv);
+    }
+
+    @Override
+    public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, IllegauxHolder holder) {
+        switch (current.getType())
+        {
+            case PAPER:
+            {
+                player.closeInventory();
+                Bukkit.dispatchCommand(player, "lp user " + player.getName() + " parent set " + holder.metiers.get(e.getSlot()).name().toLowerCase(Locale.ROOT));
+                Bukkit.dispatchCommand(player, "skin set " + holder.metiers.get(e.getSlot()).url);
+                player.sendMessage(ChatColor.RED + "Vous êtes désormais " + holder.metiers.get(e.getSlot()).name + " !");
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+    }
+
+    public enum Metiers {
+        GANGSTER("Gangster", "http://novask.in/5621013128.png")
+        ;
+
+        private final String name;
+        private final String url;
+
+        Metiers(String name, String url)
+        {
+            this.name = name;
+            this.url = url;
+        }
+
     }
 }
