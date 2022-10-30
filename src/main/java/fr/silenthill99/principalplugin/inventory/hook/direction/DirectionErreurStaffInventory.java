3d9c6ca01fc3eac6146b2399da9f68317da5b873
silenthill99 <florian.graziani@sfr.fr>
2022-10-30 13:32:57 +0100

    Fin d'optimisation du menu des erreurs staff

--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/direction/DirectionErreurStaffInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/direction/DirectionErreurStaffInventory.java
@@ -41,24 +41,13 @@ public class DirectionErreurStaffInventory extends AbstractInventory<DirectionEr
 			inv.setItem(slot++, e_s);
 
 		}
-        inv.setItem(2, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Non-respect | Règlement staff").toItemStack());
-        inv.setItem(3, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Non-respect d'autrui").toItemStack());
-        inv.setItem(4, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Abus de pouvoir").setLore("Passible d'un dérank immédiat").toItemStack());
-        inv.setItem(5, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Abus de permissions").toItemStack());
-        inv.setItem(6, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "FreeBan").toItemStack());
-        inv.setItem(7, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "FreeWarn").toItemStack());
-        inv.setItem(8, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Absence non justifiée").toItemStack());
-        inv.setItem(9, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Favoritisme").setLore("A ne pas confondre avec l'attribution de circonstances atténuantes").toItemStack());
-        inv.setItem(10, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Divulgations d'infos confidencielles").toItemStack());
-        inv.setItem(11, new ItemBuilder(Material.REDSTONE).setName(ChatColor.DARK_RED + "Corruption").toItemStack());
         inv.setItem(18, RETOUR);
         inv.setItem(22, new ItemBuilder(Material.PLAYER_HEAD).setSkullOwner(target.getName()).setName(target.getName()).toItemStack());
         p.openInventory(inv);
 	}
 	
 	@Override
-	public void manageInventory(InventoryClickEvent e, ItemStack current, Player player,
-			DirectionErreurStaffHolder holder) {
+	public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, DirectionErreurStaffHolder holder) {
         OfflinePlayer target = holder.getTarget();
 		ErreurStaff erreur_staff = holder.erreur_staff.get(e.getSlot());
 
@@ -81,7 +70,16 @@ public class DirectionErreurStaffInventory extends AbstractInventory<DirectionEr
 	{
 		FLY_SANS_VANISH("Fly sans vanish"),
 		GOD_EN_WZ("God en WZ", "Uniquement si vanish désactivé !"),
-		NON_RESPECT_REGLEMENT_STAFF("Non-respect | Règlement staff")
+		NON_RESPECT_REGLEMENT_STAFF("Non-respect | Règlement staff"),
+		NON_RESPECT_DAUTRUI("Non-respect d'autrui"),
+		ABUS_DE_POUVOIR("Abus de pouvoir", "Passible d'un dérank immédiat"),
+		ABUS_DE_PERMISSIONS("Abus de permissions"),
+		FREE_BAN("FreeBan"),
+		FREE_WARN("FreeWarn"),
+		ABSENCE_NON_JUSTIFIEE("Absence non justifiée"),
+		FAVORITISME("Favoritisme", "A ne pas confondre avec l'attribution de circonstances atténuantes"),
+		DIVULGATION("Divulgations d'infos confidencielles"),
+		CORRUPTION("Corruption")
 		;
 
 		private final String titre;
