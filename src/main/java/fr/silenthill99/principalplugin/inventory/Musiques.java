6e7ebb91a39e0612356b8ae8b2405ca09ae7a301
silenthill99 <florian.graziani@sfr.fr>
2023-07-17 19:19:36 +0200

    Rajout du thème d'Oshi no ko dans les musiques

--- src/main/java/fr/silenthill99/principalplugin/inventory/Musiques.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/Musiques.java
@@ -5,6 +5,7 @@ public enum Musiques
 	GANGSTAS_PARADISE("Gangsta's Paradise"),
     HOTEL_CALIFORNIA("Hotel California"),
     ON_MELANCHOLY_HILL("On Melancholy Hill"),
+	OSHI_NO_KO_IDOL("Oshi no ko - Idol"),
     POKEMON("Pokemon"),
     PUMPED_UP_KICKS("Pumped Up Kicks"),
     VIVA_LA_VIDA("Viva la vida");
