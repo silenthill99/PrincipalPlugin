7314b69a743f18c33a881bfc523144a8caf423f5
silenthill99 <florian.graziani@sfr.fr>
2024-07-06 23:13:56 +0200

    Finition de la partie "dépot" des inventaires + réécriture de ce dernier

--- src/main/java/fr/silenthill99/principalplugin/inventory/holder/DistributeurHolder.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/holder/DistributeurHolder.java
@@ -1,7 +1,12 @@
 package fr.silenthill99.principalplugin.inventory.holder;
 
 import fr.silenthill99.principalplugin.inventory.SilenthillHolder;
+import fr.silenthill99.principalplugin.inventory.hook.DistributeurInventory.Distributeur;
+
+import java.util.HashMap;
 
 public class DistributeurHolder extends SilenthillHolder {
+
+    public HashMap<Integer, Distributeur> distributeur = new HashMap<>();
 
 }
--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/DistributeurInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/DistributeurInventory.java
@@ -1,8 +1,10 @@
 package fr.silenthill99.principalplugin.inventory.hook;
 
 import fr.silenthill99.principalplugin.ItemBuilder;
+import fr.silenthill99.principalplugin.Main;
 import fr.silenthill99.principalplugin.inventory.AbstractInventory;
 import fr.silenthill99.principalplugin.inventory.holder.DistributeurHolder;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -20,11 +22,14 @@ public class DistributeurInventory extends AbstractInventory<DistributeurHolder>
 	@Override
 	public void openInventory(Player p, Object... args) {
 		p.sendMessage(ChatColor.RED + "Attention ! Système en bêta");
-		Inventory inv = createInventory(new DistributeurHolder(), 27, "Distributeur");
-		inv.setItem(10,
-				new ItemBuilder(Material.GOLD_INGOT).setName(ChatColor.GREEN + "Déposer de l'argent").toItemStack());
-		inv.setItem(16,
-				new ItemBuilder(Material.GOLD_INGOT).setName(ChatColor.GREEN + "Retirer de l'argent").toItemStack());
+		DistributeurHolder holder = new DistributeurHolder();
+
+		Inventory inv = createInventory(holder, 27, "Distributeur");
+
+		for (Distributeur distributeur : Distributeur.values()) {
+			inv.setItem(distributeur.getSlot(), new ItemBuilder(Material.GOLD_INGOT)
+					.setName(ChatColor.GREEN + distributeur.getName()).toItemStack());
+		}
 
 		inv.setItem(inv.getSize() - 1, CLOSE);
 		p.openInventory(inv);
@@ -32,21 +37,57 @@ public class DistributeurInventory extends AbstractInventory<DistributeurHolder>
 
 	@Override
 	public void manageInventory(InventoryClickEvent e, ItemStack current, Player player, DistributeurHolder holder) {
+
+		Distributeur distributeur = holder.distributeur.get(e.getSlot());
+
 		if (!current.getType().equals(Material.GOLD_INGOT))
 			return;
 
-		if (current.getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Déposer de l'argent")) {
-			if (player.getInventory().contains(ItemBuilder.getArgent())) {
-				player.getInventory().removeItem(ItemBuilder.getArgent());
-				Bukkit.dispatchCommand(player, "eco give "+ player.getName() + " 100");
-				player.sendMessage(ChatColor.GREEN + "Vous avez déposé 100€ sur votre compte !");
-			} else {
-				player.sendMessage(ChatColor.RED + "Vous n'avez pas d'argent sur vous !");
+		if (distributeur.equals(Distributeur.DEPOT)) {
+
+			int argent = 0;
+			for (ItemStack item : player.getInventory().getContents()) {
+				if (item != null && item.isSimilar(ItemBuilder.getArgent())) {
+					argent += item.getAmount();
+				}
 			}
-		} else if (current.getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Retirer de l'argent")) {
-			player.getInventory().addItem(ItemBuilder.getArgent());
-			Bukkit.dispatchCommand(player, "eco take " + player.getName() + " 100");
-			player.sendMessage(ChatColor.GREEN + "Vous avez retiré 100€ !");
+			if (argent == 0) {
+				player.sendMessage(Component.text(ChatColor.RED + "Vous n'avez pas d'argent sur vous !"));
+			} else {
+				Main.getEconomy().depositPlayer(player, 100 * argent);
+				player.getInventory().remove(ItemBuilder.getArgent(argent));
+				player.sendMessage(Component.text(ChatColor.GREEN + "Vous avez déposé " + 100*argent + "€"));
+			}
+		} else if (distributeur.equals(Distributeur.RETRAIT)) {
+			if (Main.getEconomy().has(player, 100)) {
+				player.getInventory().addItem(ItemBuilder.getArgent());
+				Bukkit.dispatchCommand(player, "eco take " + player.getName() + " 100");
+				player.sendMessage(ChatColor.GREEN + "Vous avez retiré 100€ !");
+			} else {
+				player.sendMessage(Component.text(ChatColor.RED + "Vous n'avez pas assez d'argent sur votre" +
+						" compte !"));
+			}
+		}
+	}
+
+	public enum Distributeur {
+		DEPOT(10, "Déposer de l'argent"),
+		RETRAIT(16, "Retirer de l'argent");
+
+		private final int slot;
+		private final String name;
+
+		Distributeur(int slot, String name) {
+			this.slot = slot;
+			this.name = name;
+		}
+
+		public int getSlot() {
+			return slot;
+		}
+
+		public String getName() {
+			return name;
 		}
 	}
 }
