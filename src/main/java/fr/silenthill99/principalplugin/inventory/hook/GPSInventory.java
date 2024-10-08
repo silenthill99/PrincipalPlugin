7ef5bc896db4932083cde7eb0d59a212b0b2ba01
BuildTools <unconfigured@null.spigotmc.org>
2024-10-08 16:35:17 +0200

    Reformat du code

--- src/main/java/fr/silenthill99/principalplugin/inventory/hook/GPSInventory.java
+++ src/main/java/fr/silenthill99/principalplugin/inventory/hook/GPSInventory.java
@@ -99,13 +99,12 @@ public class GPSInventory extends AbstractInventory<GPSHolder> {
         return direction;
     }
 
-    public enum Gps
-    {
-
+    public enum Gps {
         HOPITAL("Hôpital", new Location(world, 31.15, 64.0, 65.24)),
         MAGASIN_DE_MUSIQUE("Magasin de musique", new Location(world, -32.5, 64, 229.5)),
         MAIRIE("Mairie", new Location(world,-141.9, 64.0, -43.9)),
         POLE_EMPLOI("Pôle emploi", new Location(world, -148.3, 64.0, 10.4));
+
         private final String name;
         private final Location coord;
 
